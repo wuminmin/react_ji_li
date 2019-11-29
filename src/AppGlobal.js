67203416db71f2742252cc6e4584a68165097155
admin <you@example.com>
2019-11-29 13:34:51 +0800

    134.64.248.147内网部署

--- src/AppGlobal.js
+++ src/AppGlobal.js
@@ -1,20 +1,39 @@
 class AppGlobal {
+    // url = {
+    //     javaurl_login: 'http://114.116.152.155:8080/role/login',
+    //     django:'https://wx.wuminmin.top/jilizhushou',
+    //     index:'http://weapp.wuminmin.top',
+    //     login:'http://weapp.wuminmin.top/login',
+    //     wenzhang:'http://weapp.wuminmin.top/wenzhang',
+    //     duixian: 'http://weapp.wuminmin.top/duixian',
+    //     queren:'http://weapp.wuminmin.top/queren',
+    //     upload:'https://wx.wuminmin.top/jilizhushou/upload',
+    //     // upload:'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    //     myurl2:'http://weapp.wuminmin.top?usertoken=123456',
+    //     get_tittle_list:'https://wx.wuminmin.top/jilizhushou/get_tittle_list',
+    //     getUserInfo:'https://wx.wuminmin.top/jilizhushou/get_user_info',
+    //     rd_xia_zai_by_lan_mu:'https://wx.wuminmin.top/jilizhushou/rd_xia_zai_by_lan_mu',
+    //     rd_xia_zai_by_tittle:'https://wx.wuminmin.top/jilizhushou/rd_xia_zai_by_tittle',
+    //     que_ren_by_sellid:'https://wx.wuminmin.top/jilizhushou/que_ren_by_sellid',
+    // };
+
     url = {
         javaurl_login: 'http://114.116.152.155:8080/role/login',
         django:'https://wx.wuminmin.top/jilizhushou',
-        index:'http://weapp.wuminmin.top',
-        login:'http://weapp.wuminmin.top/login',
-        wenzhang:'http://weapp.wuminmin.top/wenzhang',
-        duixian: 'http://weapp.wuminmin.top/duixian',
-        queren:'http://weapp.wuminmin.top/queren',
         upload:'https://wx.wuminmin.top/jilizhushou/upload',
-        // upload:'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-        myurl2:'http://weapp.wuminmin.top?usertoken=123456',
         get_tittle_list:'https://wx.wuminmin.top/jilizhushou/get_tittle_list',
         getUserInfo:'https://wx.wuminmin.top/jilizhushou/get_user_info',
         rd_xia_zai_by_lan_mu:'https://wx.wuminmin.top/jilizhushou/rd_xia_zai_by_lan_mu',
         rd_xia_zai_by_tittle:'https://wx.wuminmin.top/jilizhushou/rd_xia_zai_by_tittle',
         que_ren_by_sellid:'https://wx.wuminmin.top/jilizhushou/que_ren_by_sellid',
+
+        index:'http://134.64.248.147:18090',
+        login:'http://134.64.248.147:18090/login',
+        wenzhang:'http://134.64.248.147:18090/wenzhang',
+        duixian: 'http://134.64.248.147:18090/duixian',
+        queren:'http://134.64.248.147:18090/queren',
+        myurl2:'http://134.64.248.147:18090?usertoken=123456',
+       
     };
     
 }
